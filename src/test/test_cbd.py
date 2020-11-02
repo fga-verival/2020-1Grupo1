07afc867d8fcbd42b32ad9dec844913e285ecea3
lorryaze <lorrany90@gmail.com>
2020-11-02 20:34:03 -0300

    falta de implementação - teste de imposto de renda

--- src/test/test_cbd.py
+++ src/test/test_cbd.py
@@ -19,4 +19,8 @@ class TestClass:
 
     def teste_rendimento_liquido(self):
         cbd = Cbd(1400,8.5,60)
-        assert (500 == cbd.rendimento_liquido()
+        assert (500 == cbd.rendimento_liquido())
+
+    def teste_imposto_de_renda(self):
+        cbd = Cbd(1400,8.5,60)
+        assert cbd.rendimento_bruto() * ((100 * ( cbd.imposto_de_renda() / cbd.rendimento_bruto()))/100) == cbd.imposto_de_renda()
