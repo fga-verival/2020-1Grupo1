05199b72c8ee6381dbb148a586949151c634dd80
Gabriel <gabrielfilipe7unb@gmail.com>
2020-11-02 23:50:36 -0300

    5o_commit-rendimento_liquido

--- src/test/test_cbd.py
+++ src/test/test_cbd.py
@@ -11,7 +11,7 @@ class TestClass:
 
     def teste_rendimento_bruto(self):
         cbd = Cbd(1400,8.5,60)
-        assert (18.90102 == round(cbd.rendimento_bruto(), 5)) # utilizando round para testar com 5 casas decimais
+        assert (500 == round(cbd.rendimento_bruto(), 5)) # utilizando round para testar com 5 casas decimais
 
     def teste_rendimento_bruto_2(self):
         cbd = Cbd(1000,8.5,60)
@@ -20,7 +20,7 @@ class TestClass:
     def teste_rendimento_bruto_3(self):
         cbd = Cbd(1000,8.5,60)
         cbd2 = Cbd(1400,8.5,60)
-        assert (32,40175597635323 == cbd.rendimento_bruto() + cbd2.rendimento_bruto()) # utilizando round para testar com 5 casas decimais
+        assert (32.40175597635323 == cbd.rendimento_bruto() + cbd2.rendimento_bruto()) # utilizando round para testar com 5 casas decimais
 
     def teste_imposto_de_renda(self):
         cbd = Cbd(1400,8.5,60)
@@ -42,3 +42,8 @@ class TestClass:
     def teste_rendimento_liquido_2(self):
         cbd = Cbd(1000,8.5,60)
         assert (1.3500731656813798 == cbd.rendimento_liquido())
+
+    def teste_rendimento_liquido_3(self):
+        cbd = Cbd(1000,8.5,60)
+        cbd2 = Cbd(1400,9,120)
+        assert (4.223830471538108 == cbd.rendimento_liquido()+ cbd2.rendimento_liquido())
